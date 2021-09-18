636051ca5eda8cc27096150f6036c3d1a6c6c0a3
ryan-ktech <77474794+ryan-ktech@users.noreply.github.com>
2021-09-18 18:19:35 -0500

    Initial commit

---
+++ src/main/java/net/ryanthames/randomep/RandomepApplication.java
@@ -0,0 +1,13 @@
+package net.ryanthames.randomep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ryanthames/randomep/RandomepApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ryanthames.randomep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
